ec620065faa31f8f7c292d47c436f363e4ef6ab3
mikheii <H3711@LABRANET.JAMK.FI>
2016-03-30 15:08:06 +0300

    added code

--- js/library.js
+++ js/library.js
@@ -1,10 +1,21 @@
 //document ready
 $("document").ready(function(e) {
     
-    // books image event handling
+    // books image click event
     $("#books").click(function (e) {
        $(".text").load("../books.html") 
     });
+    
+    // open image click event 
+    $("#open").click(function (e) {
+       $(".text").load("../open.html") 
+    });
+    
+    // computer image click event
+    $("#computer").click(function (e) {
+       $(".text").load("../computer.html") 
+    });
+    
     
     
 });
