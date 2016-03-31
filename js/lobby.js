3606e92304f6115e0e24a3d59aed4bb1687765e2
inffi <k1817@student.jamk.fi>
2016-03-31 13:36:02 +0300

    added labels to lobby

--- js/lobby.js
+++ js/lobby.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 
   $('#highlight_wrap').children().mouseover(function(){
+    var $thistext = "#" + $(this).attr("data-label");
+    $($thistext).off();
     $(this).addClass('is-visible');
+    $($thistext).addClass('label_highlight')
     $(this).mouseleave(function(){
       $(this).removeClass('is-visible');
+      $($thistext).removeClass('label_highlight')
     });
   });
 
