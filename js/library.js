8b749a46bd985239e9ca5dc722da1b584da6b788
mikheii <H3711@LABRANET.JAMK.FI>
2016-04-13 14:03:08 +0300

    kirjaston jsn siistimistä

--- js/library.js
+++ js/library.js
@@ -1,17 +1,17 @@
 //document ready
 $("document").ready(function(e) {
     
-    // books image click event
+    // books(Lainattavat)image click event
     $("#books").click(function (e) {
        $(".text").load("../pages/books.html") 
     });
     
-    // open image click event 
+    //  open(aukioloajat)image click event 
     $("#open").click(function (e) {
        $(".text").load("../pages/open.html") 
     });
     
-    // computer image click event
+    // computer(Linkit)image click event
     $("#computer").click(function (e) {
        $(".text").load("../pages/computer.html") 
     });
