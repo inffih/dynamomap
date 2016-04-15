c926b14d3359e31d125af9d125361d0497063590
inffi <k1817@student.jamk.fi>
2016-04-15 09:56:16 +0300

    Changed file paths

--- app/js/floors.js
+++ app/js/floors.js
@@ -1,19 +1,19 @@
 $("document").ready(function(e) {
 
   $("#floorbutton2").click(function (e) {
-     $(".content").load("../pages/floor2.html");
+     $(".content").load("./pages/floor2.html");
   });
 
   $("#floorbutton3").click(function (e) {
-     $(".content").load("../pages/floor3.html");
+     $(".content").load("./pages/floor3.html");
   });
 
   $("#floorbutton4").click(function (e) {
-     $(".content").load("../pages/floor4.html");
+     $(".content").load("./pages/floor4.html");
   });
 
   $("#floorbutton5").click(function (e) {
-     $(".content").load("../pages/floor5.html");
+     $(".content").load("./pages/floor5.html");
   });
 
 });
--- app/js/restaurant.js
+++ app/js/restaurant.js
@@ -1,18 +1,18 @@
 function kokkiMouseOver() {
   var image = document.getElementById('cook');
-  if (image.src.match("../img/cook_colourless.png")) {
-    image.src="../img/Cook_colored.png";
+  if (image.src.match("./img/cook_colourless.png")) {
+    image.src="./img/Cook_colored.png";
   } else {
-    image.src = "../img/Cook_colourless.png";
+    image.src = "./img/Cook_colourless.png";
   }
 }
 
 function kokkiMouseOut() {
   var image = document.getElementById('cook');
   if (image.src.match("../img/cook_colored.png")) {
-  image.src="../img/Cook_colored.png";
+  image.src="./img/Cook_colored.png";
   } else {
-  image.src = "../img/cook_colourless.png";
+  image.src = "./img/cook_colourless.png";
   }
 }
 
