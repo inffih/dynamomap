8062c56c2169a5b0a59bc26ec4f9231d13f62cc6
Maiju Schreck <K2322@student.jamk.fi>
2016-04-19 14:09:36 +0300

    Jquery Dynamitti speasks
    + restaurant back button

--- app/js/floors.js
+++ app/js/floors.js
@@ -3,18 +3,22 @@ $("document").ready(function(e) {
   // navigation inside floors page
   $("#floorbutton2").click(function (e) {
      $(".content").load("pages/floor2.html");
+      $(".bubble").children().html("LAITA TÄHÄN OMAN KERROKSEN TEKSTISI FLOORS.JS TIEDOSTOON");
   });
   
   $("#floorbutton3").click(function (e) {
      $(".content").load("pages/floor3.html");
+    $(".bubble").children().html("Inssit ja sossut ne yhteen soppii!");
   });
 
   $("#floorbutton4").click(function (e) {
      $(".content").load("pages/floor4.html");
+       $(".bubble").children().html("LAITA TÄHÄN OMAN KERROKSEN TEKSTISI FLOORS.JS TIEDOSTOON");
   });
 
   $("#floorbutton5").click(function (e) {
      $(".content").load("pages/floor5.html");
+       $(".bubble").children().html("LAITA TÄHÄN OMAN KERROKSEN TEKSTISI FLOORS.JS TIEDOSTOON");
   });
 
   // bubble animation
