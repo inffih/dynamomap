d772fd63af735acdd37fdbf33305e197470a705a
K2218 <K2218@student.jamk.fi>
2016-04-22 08:13:44 +0300

    final

--- app/js/floor2.js
+++ app/js/floor2.js
@@ -1,6 +1,15 @@
-// var x = document.getElementById('sound');
-// document.getElementById('volume').addEventListener('click',function(e) {
-//     e=e | window.event;
-//     x.muted = !x.muted;
-//     event.preventDefault()
-// }, false);
+$(document).ready(function(e){
+    
+            $(".left_content, #leftback").click(function(e) {
+                $(".leftpopup").fadeToggle(1000);
+            });
+
+            $(".right_content, #rightback").click(function(e) {
+                $(".rightpopup").fadeToggle(1000);
+            });
+
+            $(".bottom_content, #bottomback").click(function(e) {
+                $(".bottompopup").fadeToggle(1000);
+            }); 
+
+    });
